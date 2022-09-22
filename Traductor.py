8045ae6c1950fe96aa535e400bd10bfe2abfa4ed
Octabius141-Raúl Fortea <raulfortea@protonmail.com>
2022-09-22 11:48:39 +0200

    22/09/2022 v1

--- Traductor.py
+++ Traductor.py
@@ -1,9 +1,32 @@
 "Hay que hacer un programa que "
 
 from cmd import PROMPT
+import math
+import sys 
 
- 
-lenguaje_1 = input("> ¿Cual es el lenguaje que vas a introducir? (Dec, bin, octa, hex)\n>")
-lenguaje_2 = input("> ¿A que lenguaje quieres traducir? ((Dec, bin, octa, hex)\n>")
+while True:
+    lenguaje_1 = int((input("> ¿Cual es el lenguaje que vas a introducir, introduce el número correspondiente? ((1)dec, (2)bin, (3)octa,(4)hex)\n> ")))
+    lenguaje_2 = int((input("> ¿A que lenguaje quieres traducir, introduce el número correspondiente? ((1)dec, (2)bin, (3)octa, (4)hex)\n> ")))
 
-def traduce
+    if lenguaje_1 == (1 or 2 or 3 or 4) and lenguaje_2 == (1 or 2 or 3 or 4):
+        break
+    else:
+        print("No ha introducido un valor valido")
+
+n_decimal,n_entero = (math.modf(float(input(">¿Qué numero quieres traducir?\n> "))))
+
+
+n_decimal = round(n_decimal,5)
+
+
+"Entonces una vez se haya elegido los lenguajes, utilizamos sentencias de if anidadas para mostrar "
+if lenguaje_1 == "bin" :
+    print("El lenguaje con el que vas a introducir el lenguaje es binario")
+    if lenguaje_2 == "dec":
+        print("El lenguaje resultante va a ser decimal")
+        
+
+
+        
+
+
